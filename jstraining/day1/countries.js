1a8471102b1d6ea885a62c459b340f356e8992d5
Parameswari Bala <parameswaribala@gmail.com>
2024-09-23 15:50:13 +0530

    updated

--- jstraining/day1/countries.js
+++ jstraining/day1/countries.js
@@ -1,32 +1,35 @@
 function getCountryData(methodName,url){
+    var xhr = new XMLHttpRequest();
+    var promiseObj=new Promise(success => {
 
-    var promiseObj=new Promise(function(success,failure){
-        //ajax call
-        var xhr=new XMLHttpRequest();
-        xhr.open(methodName,url,true );
-        xhr.send()
+            xhr.open(methodName, url, true);
+            xhr.send()
 
-        //retrieve response
-        xhr.onreadystatechange=function(){
-            if (xhr.readyState === 4){
-                if (xhr.status === 200){
-                    console.log("xhr done successfully");
-                    var resp = xhr.responseText;
-                    var respJson = JSON.parse(resp);
-                    success(respJson);
+            //retrieve response
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        console.log("xhr done successfully");
+                        var resp = xhr.responseText;
+                        var respJson = JSON.parse(resp);
+                        success(respJson);
+                    }
                 } else {
-                    failure(xhr.status);
-                    console.log("xhr failed");
+                    console.log("xhr processing going on");
                 }
-            } else {
-                console.log("xhr processing going on");
             }
+        },
+        failure=>{
 
+                failure(xhr.status);
+                console.log("xhr failed");
 
         }
-
-
-    })
+    );
 
     return promiseObj;
 }
+
+
+
+
