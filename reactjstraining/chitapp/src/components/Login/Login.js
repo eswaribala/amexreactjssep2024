e85dd61ded6a88493353e68a3af4cfb2d50df714
Parameswari Bala <parameswaribala@gmail.com>
2024-09-25 14:38:38 +0530

    updated

--- reactjstraining/chitapp/src/components/Login/Login.js
+++ reactjstraining/chitapp/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, Button, TextField} from "@mui/material";
 import './Login.css'
 import {CustomTextField} from "../atoms/CustomTextField";
@@ -7,6 +7,15 @@ import  * as yup from 'yup';
 import {useFormik} from "formik";
 
 function Login(){
+
+    //value state
+    const [value,setValue]=useState('');
+
+    useEffect((value) => {
+        setValue(value)
+
+    }, [value]);
+
 
     const yupValidationSchema=yup.object({
         email:yup.string("Enter Email").email("Enter Valid Email").required("Must Enter email"),
@@ -32,9 +41,22 @@ function Login(){
             <form>
                 <fieldset>
                     <legend className="legend">Customer Login</legend>
-                    <CustomTextField id="email" label="Email"/>
-                    <CustomTextField id="password" label="Password"/>
-                    <CustomTextField id="captcha" label="captcha"/>
+                    <CustomTextField id="email" label="Email" type="email" value={formik.values.email}
+                                     onBlur={formik.handleBlur}
+                                     onChange={formik.handleChange}
+                                     error={formik.errors.email && Boolean(formik.errors.email)}
+                                     helperText={formik.touched.email && formik.errors.email}
+
+                    />
+                    <CustomTextField id="password" label="Password" type="password" value={formik.values.password}
+                                     onBlur={formik.handleBlur}
+                                     onChange={formik.handleChange}
+                                     error={formik.errors.password && Boolean(formik.errors.password)}
+                                     helperText={formik.touched.password && formik.errors.password}
+
+
+                    />
+                   {/* <CustomTextField id="captcha" label="captcha"/>*/}
                     <CustomButton text="Login" color="success" variant="contained" ></CustomButton>
                 </fieldset>
             </form>
--- reactjstraining/chitapp/src/components/atoms/CustomTextField.js
+++ reactjstraining/chitapp/src/components/atoms/CustomTextField.js
@@ -2,10 +2,15 @@ import React from 'react'
 import {TextField} from "@mui/material";
 
 
-export function CustomTextField({id,label}){
+export function CustomTextField({id,label,type,value,onBlur,onChange,error,helperText}){
 
     return(
-        <TextField id={id}  label={label} fullWidth variant="outlined" sx={{mb:2}}>
+        <TextField id={id}  label={label} type={type} value={value}
+                   onBlur={e=>onBlur(e)}
+                   onChange={e=>onChange(e)}
+                   error={error}
+                   helperText={helperText}
+                   fullWidth variant="outlined" sx={{mb:2}}>
 
         </TextField>
     )
